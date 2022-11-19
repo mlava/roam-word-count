abede87480d7549f1e73f2aea03424ca64c9f9cc
Mark Lavercombe <mlava@users.noreply.github.com>
2022-11-20 10:37:29 +1100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ export default {
             label: "Word Count (selected text only)",
             callback: () => getSelectionText(),
         });
+        window.roamAlphaAPI.ui.blockContextMenu.addCommand({
+            label: "Word Count (selected block(s) only)",
+            callback: (e) => getSelectionText(e),
+        });
 
         myEventHandler = function (e) {
             if (e.code === 'KeyQ' && e.altKey && e.shiftKey) {
@@ -27,39 +31,58 @@ export default {
         window.roamAlphaAPI.ui.commandPalette.removeCommand({
             label: 'Word Count (selected text only)'
         });
+        window.roamAlphaAPI.ui.blockContextMenu.removeCommand({
+            label: "Word Count (selected block(s) only)",
+            callback: (e) => getSelectionText(e),
+        });
         window.removeEventListener('keydown', myEventHandler, false);
     }
 }
 
 // get selection text
-async function getSelectionText() {
+async function getSelectionText(e) {
     let uids = await roamAlphaAPI.ui.individualMultiselect.getSelectedUids();
-    if (uids.length === 0) {
+    var text = "";
+    var words = "";
+    var wordsCount = 0;
+
+    if (e) { // block context menu
+        if (uids.length === 0) { // one block only
+            text += e["block-string"].toString().trim();
+            console.info("Text: ",text);
+        } else { // block multi-select mode
+            for (var i = 0; i < uids.length; i++) {
+                var results = await window.roamAlphaAPI.data.pull("[:block/string]", [":block/uid", uids[i]]);
+                text += results[":block/string"].toString().trim()+" ";
+            }
+            console.info("Text: ",text);
+        }
+    } else if (uids.length === 0) { // command palette and not multi-select mode
         wordCount(true);
         return;
-    } else {
-        var wordsCount = 0;
+    } else { // block multi-select mode and command palette
         for (var i = 0; i < uids.length; i++) {
             var results = await window.roamAlphaAPI.data.pull("[:block/string]", [":block/uid", uids[i]]);
-            var refString = results[":block/string"].toString().trim();
-            if (refString != "") {
-                var words = refString.split(" ").length;
-                wordsCount = wordsCount + words;
-            }
+            text += results[":block/string"].toString().trim()+" ";
         }
-        if (wordsCount == 1) {
-            wordsCount = "N/A";
-        }
-        iziToast.show({
-            theme: 'dark',
-            message: wordsCount + ' words in selected text',
-            position: 'center',
-            close: false,
-            timeout: 5000,
-            closeOnClick: true,
-            displayMode: 2
-        });
+        console.info("Text: ",text);
     }
+    if (text != "") {
+        words = text.split(" ").length;
+        wordsCount = wordsCount + words;
+    }
+    if (wordsCount == 1) {
+        wordsCount = "N/A";
+    }
+    iziToast.show({
+        theme: 'dark',
+        message: wordsCount + ' words in selected text',
+        position: 'center',
+        close: false,
+        timeout: 5000,
+        closeOnClick: true,
+        displayMode: 2
+    });
 }
 
 async function wordCount(selected) {
@@ -105,7 +128,7 @@ async function wordCount(selected) {
     }
     var toast = "";
     toast += wordCount + " words on this page";
-    if (selected) { 
+    if (selected) {
         toast += "  (No selected text!)";
     }
 
